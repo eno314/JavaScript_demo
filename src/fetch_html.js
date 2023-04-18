9fbf8042dd36e9e921ffc9ad8402df4c62ddfa9b
Hiroto Kitamura <kirikonknow@gmail.com>
2023-04-18 14:08:18 +0900

    fix fetchHTML to be simple.

--- src/fetch_html.js
+++ src/fetch_html.js
@@ -1,13 +1,8 @@
 export async function fetchHTML(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const html = await response.text();
-    return html;
-  } catch (error) {
-    console.error("Error fetching HTML:", error);
-    throw error;
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
   }
+  const html = await response.text();
+  return html;
 }
